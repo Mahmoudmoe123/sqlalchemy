85793adde571773fe81757eb42e5d56df43ff1b5
Mahmoud Mohamed <mahmoudmoe213@gmail.com>
2023-04-11 01:13:03 -0400

    alchemy upload try

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, abort, redirect, render_template, request
 
 from src.repositories.movie_repository import movie_repository_singleton
+from flask_sqlalchemy import SQLAlchemy
+from models import db  # Import the db object from models.py
 
 app = Flask(__name__)
 
 # TODO: DB connection
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgress:notrealpassword@localhost/movies'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)  # Initialize the db object with the app instance
+
 
 @app.get('/')
 def index():
--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -1,21 +1,28 @@
+from models import Movie, db
+
 class MovieRepository:
 
     def get_all_movies(self):
-        # TODO get all movies from the DB
-        return None
+        # Get all movies from the DB
+        movies = Movie.query.all()
+        return movies
 
     def get_movie_by_id(self, movie_id):
-        # TODO get a single movie from the DB using the ID
-        return None
+        # Get a single movie from the DB using the ID
+        movie = Movie.query.get(movie_id)
+        return movie
 
     def create_movie(self, title, director, rating):
-        # TODO create a new movie in the DB
-        return None
+        # Create a new movie in the DB
+        new_movie = Movie(title=title, director=director, rating=rating)
+        db.session.add(new_movie)
+        db.session.commit()
+        return new_movie
 
     def search_movies(self, title):
-        # TODO get all movies matching case insensitive substring (SQL LIKE, use google for how to do with SQLAlchemy)
-        return None
-
+        # Get all movies matching case insensitive substring (SQL LIKE, use google for how to do with SQLAlchemy)
+        movies = Movie.query.filter(Movie.title.ilike(f"%{title}%")).all()
+        return movies
 
 # Singleton to be used in other modules
 movie_repository_singleton = MovieRepository()
